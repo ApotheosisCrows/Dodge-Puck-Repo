48822f705349b1041a5debdf1b3143e9480bf470
ApotheosisCrows <90779268+ApotheosisCrows@users.noreply.github.com>
2021-10-11 09:24:25 -0400

    Update ?

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -3,28 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Testing : MonoBehaviour
-{ 
+{
     public float speed;
     public float xRange;
     public float yRange;
-
+    public GameObject Puck;
+    public GameObject Pickup;
+    public GameObject scoreText;
     // Start is called before the first frame update
     void Start()
     {
-
-
+        Instantiate(Pickup, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+     
     }
 
     // Update is called once per frame
     void Update()
-    {  
+    {
         //Keep Player within xRange(Left and Right sides)
-        if(transform.position.x > xRange)
+        if (transform.position.x > xRange)
         {
             transform.position = new Vector2(xRange, transform.position.y);
         }
-         
-        if(transform.position.x < -xRange)
+
+        if (transform.position.x < -xRange)
         {
             transform.position = new Vector2(-xRange, transform.position.y);
         }
@@ -38,6 +40,10 @@ public class Testing : MonoBehaviour
         {
             transform.position = new Vector2(transform.position.x, -yRange);
         }
+       
+        //Instantiate(Puck, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+
+     
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         Debug.Log(moveHorizontal);
@@ -53,5 +59,21 @@ public class Testing : MonoBehaviour
         //    Debug.Log(Input.GetAxis("Horizontal"));
         //    transform.Translate(Vector2.right * speed * Time.deltaTime);
         //}
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Hit Pickup");
+            Instantiate(Pickup, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+            Instantiate(Puck, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+
+            scoreText.GetComponent<Score>().UpdateScore();
+        }
+        
+
+
     }
 }
